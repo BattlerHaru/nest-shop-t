259066b18e328116b412f5048ea21561962a5ea9
BattlerHaru <kronk.sdance@gmail.com>
2024-05-04 16:17:58 -0600

    :wrench: update slug and id search to get product with queryBuilder

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,11 @@
 import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { validate as isUUID } from 'uuid';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
 import { PaginationDTO } from '../common/dtos/pagination.dto';
-import { validate as isUUID } from 'uuid';
 
 @Injectable()
 export class ProductsService {
@@ -58,7 +58,15 @@ export class ProductsService {
     if (isUUID(term)) {
       product = await this.productRepository.findOneBy({ id: term });
     } else {
-      product = await this.productRepository.findOneBy({ slug: term });
+
+      const queryBuilder = this.productRepository.createQueryBuilder();
+
+      product = await queryBuilder
+        .where(`UPPER(title) =:title or slug =:slug`, {
+          title: term.toUpperCase(),
+          slug: term.toLowerCase()
+        }).getOne()
+
     }
 
     if (!product) {
