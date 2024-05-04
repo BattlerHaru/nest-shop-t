51c0a976bde9700ff8c5f9486a735e7e3412ceb5
BattlerHaru <kronk.sdance@gmail.com>
2024-05-04 14:13:10 -0600

    :sparkles: add delete method to products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -57,8 +57,15 @@ export class ProductsService {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    const product = await this.findOne(id)
+
+    if (!product) {
+      throw new NotFoundException(`Product with id "${id}" not found`)
+    }
+
+    await this.productRepository.remove(product)
+    return `product with id "${id}" successfully removed`;
   }
 
   private handleDBExceptions(error: any) {
