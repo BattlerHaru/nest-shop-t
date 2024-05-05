f91f6783b1e5d077eb383b989246bcef9cc87264
BattlerHaru <kronk.sdance@gmail.com>
2024-05-05 15:22:08 -0600

    :card_file_box: relation in product column images OneToMany, in product-image column product ManyToOne - all entitys

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,5 @@
-import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { ProductImage } from "./";
 
 @Entity()
 export class Product {
@@ -52,7 +53,14 @@ export class Product {
     })
     tags: string[]
 
-    //images
+    @OneToMany(
+        () => ProductImage,
+        (productImage) => productImage.product,
+        {
+            cascade: true
+        }
+    )
+    images?: ProductImage
 
     @BeforeInsert()
     checkSlugInsert() {
