3174d439b5c62b2f45dc59e631a920abede6ae45
BattlerHaru <kronk.sdance@gmail.com>
2025-01-27 14:44:42 -0600

    :sparles: feat(auth): implement login method base in auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,43 +1,62 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcryptjs';
-import { CreateUserDto } from './dto/create-user-dto';
+import { CreateUserDto, LoginUserDto } from './dto/index';
 import { User } from './entities/user.entity';
 
 @Injectable()
 export class AuthService {
   constructor(
-    @InjectRepository(User)
+    @InjectRepository( User )
     private readonly userRepository: Repository<User>
   ) { }
 
 
-  async create(createUserDto: CreateUserDto) {
+  async create( createUserDto: CreateUserDto ) {
     try {
 
-      const { password, ...userData } = createUserDto
+      const { password, ...userData } = createUserDto;
 
-      const user = this.userRepository.create({
+      const user = this.userRepository.create( {
         ...userData,
-        password: bcrypt.hashSync(password, 10)
-      });
-      await this.userRepository.save(user)
+        password: bcrypt.hashSync( password, 10 )
+      } );
+      await this.userRepository.save( user );
       delete user.password;
-      return user
+      return user;
 
-    } catch (error) {
-      this.handleDBErrors(error)
+    } catch ( error ) {
+      this.handleDBErrors( error );
     }
   }
 
-  private handleDBErrors(error: any): never {
-    if (error.code === '23505') {
-      throw new BadRequestException(error.detail);
-    }
-    console.log(error);
+  async login( loginUserDto: LoginUserDto ) {
+    const { password, email } = loginUserDto;
 
-    throw new InternalServerErrorException('Please check server logs')
+    const user = await this.userRepository.findOne( {
+      where: { email },
+      select: { email: true, password: true }
+    } );
+
+    if ( !user ) {
+      throw new UnauthorizedException( 'Credentials are not valid (email)' );
+    }
+
+    if ( !bcrypt.compareSync( password, user.password ) ) {
+      throw new UnauthorizedException( 'Credentials are not valid (password)' );
+    }
+
+    return user;
+  }
+
+  private handleDBErrors( error: any ): never {
+    if ( error.code === '23505' ) {
+      throw new BadRequestException( error.detail );
+    }
+    console.log( error );
+
+    throw new InternalServerErrorException( 'Please check server logs' );
   }
 
 }
