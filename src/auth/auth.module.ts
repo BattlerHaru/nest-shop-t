0a666a385866acefe0e975f09c9653630db6f4fa
BattlerHaru <kronk.sdance@gmail.com>
2025-01-27 15:02:58 -0600

    :sparkles: feat(auth): add PassportModule to auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,17 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
+
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { User } from './entities/user.entity';
 
-@Module({
-  controllers: [AuthController],
-  providers: [AuthService],
+@Module( {
+  controllers: [ AuthController ],
+  providers: [ AuthService ],
   imports: [
-    TypeOrmModule.forFeature([
+    TypeOrmModule.forFeature( [
       User,
-    ])
+    ] ),
+    PassportModule.register( { defaultStrategy: 'jwt' } ),
   ],
-  exports: [TypeOrmModule]
-})
+  exports: [ TypeOrmModule ]
+} )
 export class AuthModule { }
