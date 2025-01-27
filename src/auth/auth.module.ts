4d21c1d826c6ea77d81a88b9b160d8058f9b3ffd
BattlerHaru <kronk.sdance@gmail.com>
2025-01-27 15:15:17 -0600

    :sparkles: feat(auth): add jwt-module to auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
@@ -14,6 +16,19 @@ import { User } from './entities/user.entity';
       User,
     ] ),
     PassportModule.register( { defaultStrategy: 'jwt' } ),
+    JwtModule.registerAsync( {
+      imports: [ ConfigModule ],
+      inject: [ ConfigService ],
+      useFactory: ( configService: ConfigService ) => {
+        return {
+          secret: configService.get( 'JWT_SECRET' ),
+          signOptions: {
+            expiresIn: '2h'
+          }
+        };
+      }
+    } )
+
   ],
   exports: [ TypeOrmModule ]
 } )
