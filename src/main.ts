a2925ae3034b8fe0f1e689b89dc0863223ef14bf
BattlerHaru <kronk.sdance@gmail.com>
2025-01-28 16:47:22 -0600

    :sparkles: :memo: feat(main): implement basic Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,33 @@
 import { NestFactory } from '@nestjs/core';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const logger = new Logger('bootstrap')
 
-  app.setGlobalPrefix('api');
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    })
-  );
-  await app.listen(process.env.PORT);
-  logger.log(`App running on port ${process.env.PORT}`);
+async function bootstrap() {
+    const app = await NestFactory.create( AppModule );
+    const logger = new Logger( 'bootstrap' );
+
+    app.setGlobalPrefix( 'api' );
+    app.useGlobalPipes(
+        new ValidationPipe( {
+            whitelist: true,
+            forbidNonWhitelisted: true,
+
+        } )
+    );
+
+    const config = new DocumentBuilder()
+        .setTitle( 'shop-t RESTFul API' )
+        .setDescription( 'shop-t shop endpoints' )
+        .setVersion( '1.0' )
+        .build();
+    const document = SwaggerModule.createDocument( app, config );
+    SwaggerModule.setup( 'api', app, document );
+
+
+
+    await app.listen( process.env.PORT );
+    logger.log( `App running on port ${ process.env.PORT }` );
 }
 bootstrap();
